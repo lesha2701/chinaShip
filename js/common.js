2800d10e0198f3637add69335556af6e72803fd0
Алексей Артемович <81881832+lesha2701@users.noreply.github.com>
2024-05-21 22:46:39 +0300

    123

--- js/common.js
+++ js/common.js
@@ -44,8 +44,53 @@
 // });
 
 const swiper = new Swiper(".mySwiper", {
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    }
-  });
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  }
+});
+
+document.getElementById("open-modal-btn").addEventListener("click", function() {
+  document.getElementById("my-modal").classList.add("open")
+})
+
+document.getElementById("open-modal-btn1").addEventListener("click", function() {
+  document.getElementById("my-modal").classList.add("open")
+})
+
+document.getElementById("open-modal-btn2").addEventListener("click", function() {
+  document.getElementById("my-modal").classList.add("open")
+})
+
+document.getElementById("open-modal-btn3").addEventListener("click", function() {
+  document.getElementById("my-modal").classList.add("open")
+})
+
+document.getElementById("open-modal-btn4").addEventListener("click", function() {
+  document.getElementById("my-modal").classList.add("open")
+})
+
+document.getElementById("open-modal-btn5").addEventListener("click", function() {
+  document.getElementById("my-modal").classList.add("open")
+})
+
+// Закрыть модальное окно
+document.getElementById("close-my-modal-btn").addEventListener("click", function() {
+  document.getElementById("my-modal").classList.remove("open")
+})
+
+// Закрыть модальное окно при нажатии на Esc
+window.addEventListener('keydown', (e) => {
+  if (e.key === "Escape") {
+      document.getElementById("my-modal").classList.remove("open")
+  }
+});
+
+// Закрыть модальное окно при клике вне его
+document.querySelector("#my-modal .modal__box").addEventListener('click', event => {
+  event._isClickWithInModal = true;
+});
+document.getElementById("my-modal").addEventListener('click', event => {
+  if (event._isClickWithInModal) return;
+  event.currentTarget.classList.remove('open');
+});
