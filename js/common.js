6468256e016d987d816297e4b4120cd1f3106400
Алексей Артемович <81881832+lesha2701@users.noreply.github.com>
2024-05-21 15:39:50 +0300

    123

--- js/common.js
+++ js/common.js
@@ -1,44 +1,51 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const weightInput = document.getElementById('weight');
-    const volumeInput = document.getElementById('volume');
-    const priceInput = document.getElementById('price');
-    const calculateButton = document.querySelector('.calculator__form-button');
+// document.addEventListener('DOMContentLoaded', function() {
+//     const weightInput = document.getElementById('weight');
+//     const volumeInput = document.getElementById('volume');
+//     const priceInput = document.getElementById('price');
+//     const calculateButton = document.querySelector('.calculator__form-button');
 
-    // Функция для обновления стоимости доставки
-    function updateShippingCost() {
-        const weight = parseFloat(weightInput.value.replace(',', '.'));
-        const volume = parseFloat(volumeInput.value.replace(',', '.'));
-        const price = parseFloat(priceInput.value) * 13.52;
+//     // Функция для обновления стоимости доставки
+//     function updateShippingCost() {
+//         const weight = parseFloat(weightInput.value.replace(',', '.'));
+//         const volume = parseFloat(volumeInput.value.replace(',', '.'));
+//         const price = parseFloat(priceInput.value) * 13.52;
 
-        // Ваша логика расчета стоимости доставки, например:
-        const shippingCost = weight * 0.5 + volume * 0.2 + (price + (price * 5) / 100) ;
-        console.log('Стоимость доставки:', shippingCost);
+//         // Ваша логика расчета стоимости доставки, например:
+//         const shippingCost = weight * 0.5 + volume * 0.2 + (price + (price * 5) / 100) ;
+//         console.log('Стоимость доставки:', shippingCost);
+//     }
+
+//     function handleButtonClick(event) {
+//         const target = event.target;
+//         const input = target.parentElement.querySelector('.calculator__form-input');
+//         console.log(input.value)
+//         const value = parseFloat(input.value);
+
+//         if (target.textContent === '-' && value > 0) {
+//             input.value = (value - 1).toFixed(1); // Уменьшаем значение на 1
+//         } else if (target.textContent === '+') {
+//             input.value = (value + 1).toFixed(1); // Увеличиваем значение на 1
+//         }
+
+//         // updateShippingCost(); // Обновляем стоимость доставки при изменении значений
+//     }
+
+//     // Обработчики событий для кнопок + и -
+//     const buttons = document.querySelectorAll('.calculator__form-btn');
+//     buttons.forEach(function(button) {
+//         button.addEventListener('click', handleButtonClick);
+//     });
+
+//     // Обработчик события для кнопки "Рассчитать"
+//     calculateButton.addEventListener('click', function(event) {
+//         event.preventDefault(); // Предотвращаем отправку формы
+//         updateShippingCost(); // Вызываем функцию расчета стоимости доставки
+//     });
+// });
+
+const swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
     }
-
-    function handleButtonClick(event) {
-        const target = event.target;
-        const input = target.parentElement.querySelector('.calculator__form-input');
-        console.log(input.value)
-        const value = parseFloat(input.value);
-
-        if (target.textContent === '-' && value > 0) {
-            input.value = (value - 1).toFixed(1); // Уменьшаем значение на 1
-        } else if (target.textContent === '+') {
-            input.value = (value + 1).toFixed(1); // Увеличиваем значение на 1
-        }
-
-        // updateShippingCost(); // Обновляем стоимость доставки при изменении значений
-    }
-
-    // Обработчики событий для кнопок + и -
-    const buttons = document.querySelectorAll('.calculator__form-btn');
-    buttons.forEach(function(button) {
-        button.addEventListener('click', handleButtonClick);
-    });
-
-    // Обработчик события для кнопки "Рассчитать"
-    calculateButton.addEventListener('click', function(event) {
-        event.preventDefault(); // Предотвращаем отправку формы
-        updateShippingCost(); // Вызываем функцию расчета стоимости доставки
-    });
-});
+  });
